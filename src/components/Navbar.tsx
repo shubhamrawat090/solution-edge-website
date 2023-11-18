778015a2354cbea99758a25b9b8e63ed945b16a3
Grudge69 <shubham.shubham.rawat090@gmail.com>
2023-11-18 17:44:32 +0530

    Build bug fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Menu, Transition } from "@headlessui/react";
-import { Fragment, useEffect, useRef, useState } from "react";
+import { Fragment } from "react";
 import { navlinks } from "../constants/links";
 import logoImg from "../assets/logo.png";
 
