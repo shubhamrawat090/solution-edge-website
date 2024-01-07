55992fea4420d794cd633ad6c19de8cbdab101fe
Grudge69 <shubham.shubham.rawat090@gmail.com>
2024-01-07 17:40:25 +0530

    Testing SErvices component created

--- src/components/WhatWeDo/TestingServices.tsx
+++ src/components/WhatWeDo/TestingServices.tsx
@@ -1,87 +1,82 @@
-import { RiCheckboxCircleLine, RiSpeedLine } from "react-icons/ri";
-import { IoLogoWhatsapp } from "react-icons/io";
-import { HiOutlineChatAlt2 } from "react-icons/hi";
-import { MdEmail } from "react-icons/md";
-import { Link } from "react-router-dom";
-import { email } from "../../constants/contact-info";
+import { classNames } from "../../utils/stringManipulation";
 
 const TestingServices = () => {
-  const btnTransition = "transition ease-in delay-1500";
+  const paraStyles =
+    "font-medium text-richblack-600 text-base mb-4 text-justify";
+
+  const underlinedStyles =
+    "underline underline-offset-2 decoration-yellow-5 decoration-[3px]";
+
   return (
-    <section id="testing-services">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 grid grid-cols-1 lg:grid-cols-2 gap-6 p-4">
-        <div className="col-span-2 mb-6">
-          <h2 className="text-2xl font-bold mb-4 text-center">
-            Testing Services
-          </h2>
-          <p className="mb-4 text-justify">
-            Our testing services provide the data you need to optimize the
-            production process and get your product to market quickly and
-            economically. We support the quality, performance, regulatory
-            compliance, safety, benchmarking, evaluation, validation, analysis,
-            and other requirements for products, components, raw materials,
-            sites, and facilities.
-          </p>
-          <p className="mb-4 text-justify">
-            As regulations change and technology is created or innovated, our
-            knowledge and industry expertise ensure your products and business
-            are prepared to meet evolving demands.
-          </p>
-          <p className="text-justify">
-            We help you bring your product to market quickly, responsibly, and
-            economically to keep your business continually moving ahead.
-          </p>
-        </div>
+    <section
+      id="testing-services"
+      className="grid md:grid-cols-2 lg:grid-cols-3 md:items-center mx-auto max-w-7xl"
+    >
+      {/* left section */}
+      <div className="px-4 lg:col-span-2">
+        <h1 className="text-3xl font-bold text-center md:text-left">
+          TESTING SERVICES
+        </h1>
 
-        <div className="flex items-center gap-4">
-          <RiCheckboxCircleLine className="text-caribbeangreen-500 text-4xl" />
-          <div>
-            <h3 className="text-xl font-bold">Quality Testing</h3>
-            <p>Ensuring high-quality standards for your products.</p>
-          </div>
-        </div>
+        {/* separator */}
+        <div className="h-2 w-12 bg-richblack-25 rounded-lg my-4 mx-auto md:mx-0"></div>
 
-        <div className="flex items-center gap-4">
-          <RiSpeedLine className="text-caribbeangreen-500 text-4xl" />
-          <div>
-            <h3 className="text-xl font-bold">Performance Testing</h3>
-            <p>Optimizing performance metrics for your products.</p>
-          </div>
-        </div>
+        <p className={classNames(paraStyles)}>
+          Our testing services provide the data you need to optimize the
+          production process and get your product to market quickly and
+          economically. Our testing and certification support services support
+          the quality, performance, regulatory compliance, safety, benchmarking,
+          evaluation, validation, analysis, and other requirements for{" "}
+          <span className={classNames(underlinedStyles)}>
+            products, components, raw materials, sites, and facilities
+          </span>
+          .
+        </p>
+
+        <p className={classNames(paraStyles)}>
+          As regulations change and technology is created or innovated, our
+          knowledge and industry expertise ensure your products and business are
+          prepared to meet evolving demands.
+        </p>
+
+        <p className={classNames(paraStyles)}>
+          We help you bring your product to market quickly, responsibly, and
+          economically to keep your business continually moving ahead.
+        </p>
       </div>
 
-      <div className="mt-20 flex justify-center gap-x-6">
-        <Link
-          to="/contact-us"
-          className="transition-all group flex gap-x-1 items-center justify-center rounded-md py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-caribbeangreen-400 text-white hover:bg-caribbeangreen-500"
-        >
-          <HiOutlineChatAlt2 className="h-6 w-6" />
-          Contact Us
-        </Link>
+      {/* cards section */}
+      <div className="grid gap-4 px-4">
+        {/* card */}
+        <div className="p-5 bg-yellow-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded-lg text-center md:text-left">
+          <h1 className="font-extrabold">QUALITY AND COMPLIANCE</h1>
+          <p className="mt-5 text-[0.9rem] font-medium leading-6">
+            Quality Assurance & Regulatory Compliance <br /> Our rigorous
+            procedures ensure your products not only meet but exceed industry
+            standards. We keep you ahead of regulatory changes, guaranteeing
+            compliance with evolving global standards.
+          </p>
+        </div>
 
-        <Link
-          to="https://wa.me/+918800556980"
-          target="_blank"
-          title="WhatsApp Us"
-          className={`${btnTransition} group flex gap-x-1 ring-1 items-center justify-center rounded-md py-2 px-4 text-sm font-medium focus:outline-none ring-pure-greys-200 hover:ring-white text-pure-greys-600 hover:text-white  hover:bg-[#25D366]`}
-        >
-          <IoLogoWhatsapp
-            className={`${btnTransition} h-6 w-6 fill-pure-greys-400 group-hover:fill-white`}
-          />
-          WhatsApp Us
-        </Link>
+        <div className="p-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded-lg text-center md:text-left">
+          <h1 className="font-extrabold">PERFORMANCE AND SAFETY</h1>
+          <p className="mt-5 text-[0.9rem] font-medium leading-6">
+            Performance Evaluation & Safety Assurance <br /> Thorough testing is
+            conducted to optimize product performance, ensuring functionality
+            and durability. Additionally, our rigorous safety analysis mitigates
+            risks, upholding the safety of your offerings.
+          </p>
+        </div>
 
-        <Link
-          to={`mailto:${email}`}
-          target="_blank"
-          title="Email Us"
-          className={`${btnTransition} group flex gap-x-1 ring-1 items-center justify-center rounded-md py-2 px-4 text-sm font-medium focus:outline-none ring-pure-greys-200 hover:ring-white text-pure-greys-600 hover:text-white  hover:bg-[#C71610]`}
-        >
-          <MdEmail
-            className={`${btnTransition} h-6 w-6 fill-pure-greys-400 group-hover:fill-white`}
-          />
-          Email Us
-        </Link>
+        <div className="p-5 bg-yellow-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded-lg text-center md:text-left">
+          <h1 className="font-extrabold">VALIDATION AND EFFICIENCY</h1>
+          <p className="mt-5 text-[0.9rem] font-medium leading-6">
+            Benchmarking, Validation & Efficiency <br /> We provide precise
+            benchmarking and validation processes, ensuring product efficacy and
+            compliance. Our approach focuses on efficiency to streamline the
+            validation of your products.
+          </p>
+        </div>
       </div>
     </section>
   );
