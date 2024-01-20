5424fc17dd95161fc25c0b099205a9e52b0bbf4f
Grudge69 <shubham.shubham.rawat090@gmail.com>
2024-01-20 18:32:35 +0530

    Services offered component structured

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
-    <div className="text-4xl text-pure-greys-700 flex justify-center items-center h-[42vh]">
+    <div className="text-4xl text-pure-greys-700 flex flex-col justify-center items-center h-[42vh]">
       404 Not Found.
+      <br />
+      <div className="text-xl mt-10 underline underline-offset-2 text-blue-200">
+        <Link to="/">Go to Home Page</Link>
+      </div>
     </div>
   );
 };
